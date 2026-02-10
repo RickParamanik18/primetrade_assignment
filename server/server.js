8d00cbc61ec84273be2df501044b1783826a4c3a
Rick Paramanik <rickkcir100@gmail.com>
2026-02-10 16:45:10 +0530

    hello route added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
 connectDB();
+
+app.get("/", (req, res) => {
+    res.send("hello!!");
+});
 app.listen(process.env.PORT, () =>
     console.log("Server running on port", process.env.PORT),
 );
